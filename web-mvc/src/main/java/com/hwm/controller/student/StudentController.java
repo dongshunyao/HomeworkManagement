f291efd2fb736efb4a8c528626648cb3f2e0cf4a
董舜尧 <dongshunyao@qq.com>
2020-04-03 00:56:42 +0800

    重构 controller 包，使用 @Autowired 装配对象

--- web-mvc/src/main/java/com/hwm/controller/student/StudentController.java
+++ web-mvc/src/main/java/com/hwm/controller/student/StudentController.java
@@ -1,8 +1,11 @@
 package com.hwm.controller.student;
 
 import com.hwm.jdbc.HomeworkJdbc;
+import com.hwm.jdbc.StudentJdbc;
 import com.hwm.jdbc.SubmitHomeworkJdbc;
+import com.hwm.model.Student;
 import com.hwm.model.SubmitHomework;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,16 +16,27 @@ import java.io.UnsupportedEncodingException;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+    private HomeworkJdbc homeworkJdbc;
+    private StudentJdbc studentJdbc;
+    private SubmitHomeworkJdbc submitHomeworkJdbc;
+
+    @Autowired
+    public StudentController(HomeworkJdbc homeworkJdbc, StudentJdbc studentJdbc, SubmitHomeworkJdbc submitHomeworkJdbc) {
+        this.homeworkJdbc = homeworkJdbc;
+        this.studentJdbc = studentJdbc;
+        this.submitHomeworkJdbc = submitHomeworkJdbc;
+    }
+
     @RequestMapping("/homework_list")
     public String homeworkList(HttpServletRequest req) {
-        var list = HomeworkJdbc.selectAll();
+        var list = homeworkJdbc.selectAll();
         req.setAttribute("list", list);
         return "homeworkList.jsp";
     }
 
     @RequestMapping(value = "/submit_homework", method = RequestMethod.GET)
     public String submitHomeworkPage(HttpServletRequest req) {
-        var homework = HomeworkJdbc.select(Integer.parseInt(req.getParameter("homeworkId")));
+        var homework = homeworkJdbc.select(Integer.parseInt(req.getParameter("homeworkId")));
         req.setAttribute("homework", homework);
         return "submitHomework.jsp";
     }
@@ -41,11 +55,11 @@ public class StudentController {
         submitHomework.setHomeworkTitle(req.getParameter("homeworkTitle"));
         submitHomework.setHomeworkContent(req.getParameter("homeworkContent"));
 
-        String info;
-        if (SubmitHomeworkJdbc.addSubmitHomework(submitHomework)) {
-            info = "提交作业成功";
-        } else {
-            info = "提交作业失败，请检查输入";
+        String info = "提交作业失败，请检查输入";
+        if (studentJdbc.select(submitHomework.getStudentId()) != null) {
+            if (submitHomeworkJdbc.addSubmitHomework(submitHomework)) {
+                info = "提交作业成功";
+            }
         }
 
         req.setAttribute("info", info);
